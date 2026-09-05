1771889e567931353a94db0620b0c70aacc51787
Zayaan-k <zayaan.khandakar99@gmail.com>
2026-09-05 10:54:23 -0600

    reg file implemented

--- basys3_rv32i_soc.srcs/sources_1/new/cpu/register_file.sv
+++ basys3_rv32i_soc.srcs/sources_1/new/cpu/register_file.sv
@@ -1,26 +1,52 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
 // Engineer: 
 // 
-// Create Date: 09/04/2026 03:26:57 PM
 // Design Name: 
 // Module Name: register_file
 // Project Name: 
 // Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
+module RegisterFile (
+    input  wire        clk,
+    input  wire        reset,
+    input  wire        writeEnable,
 
-module register_file(
+    input  wire [4:0]  rs1Address,
+    input  wire [4:0]  rs2Address,
+    input  wire [4:0]  rdAddress,
 
-    );
+    input  wire [31:0] rdData,
+
+    output wire [31:0] rs1Data,
+    output wire [31:0] rs2Data
+);
+
+    reg [31:0] registers [0:31];
+
+    integer i;
+
+    // x0 always prod zero
+    assign rs1Data = (rs1Address == 5'd0)
+                   ? 32'b0
+                   : registers[rs1Address];
+
+    assign rs2Data = (rs2Address == 5'd0)
+                   ? 32'b0
+                   : registers[rs2Address];
+
+
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            for (i = 0; i < 32; i = i + 1) begin
+                registers[i] <= 32'b0;
+            end
+        end
+        else if (writeEnable && (rdAddress != 5'd0)) begin
+            registers[rdAddress] <= rdData;
+        end
+    end
+
 endmodule
