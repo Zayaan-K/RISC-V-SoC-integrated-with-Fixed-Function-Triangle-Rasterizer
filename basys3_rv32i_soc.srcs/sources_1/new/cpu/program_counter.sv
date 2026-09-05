a7ec91ef5e3d77d6ed4ec6a3ffeb4890ab5556ac
Zayaan-k <zayaan.khandakar99@gmail.com>
2026-09-05 12:09:24 -0600

    program countr implemented

--- basys3_rv32i_soc.srcs/sources_1/new/cpu/program_counter.sv
+++ basys3_rv32i_soc.srcs/sources_1/new/cpu/program_counter.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
+
 // Engineer: 
 // 
 // Create Date: 09/04/2026 03:26:57 PM
@@ -8,19 +8,28 @@
 // Module Name: program_counter
 // Project Name: 
 // Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module program_counter(
+`timescale 1ns / 1ps
 
-    );
+module program_counter (
+    input  wire        clk,
+    input  wire        reset,
+    input  wire        enable,
+    input  wire [31:0] next_pc,
+
+    output reg  [31:0] pc
+);
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            pc <= 32'h0000_0000;
+        end
+        else if (enable) begin
+            pc <= next_pc;
+        end
+    end
+
 endmodule
